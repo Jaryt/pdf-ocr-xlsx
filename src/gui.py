5d6648343d325a0eef7b70497e14c0472ecbca23
Jaryt <jarytbustard@Gmail.com>
2020-11-02 18:41:50 -0500

    Complete

--- src/gui.py
+++ src/gui.py
@@ -1,17 +1,30 @@
 from tkinter import *
 from tkinter.ttk import *
+from operator import mul
+from operator import truediv as div
 from PIL import Image, ImageTk
-import json
 
 
 class GUI:
     page = 1
+    scale = (1, 1)
     selection_rect = None
-    selections = {}
+
+    def load_selections(self):
+        if str(self.page) in self.selections:
+            for selection in self.selections[str(self.page)]:
+                bounds = selection['bounds']
+                scaled_bounds = {'min': tuple(map(mul, bounds['min'], self.scale)),
+                                 'max': tuple(map(mul, bounds['max'], self.scale))}
+                new_rect, lbl_text = self.draw_rect(
+                    scaled_bounds, selection['cell'])
+
+                selection['rect'] = new_rect
+                selection['text'] = lbl_text
 
     def update_page(self, inc):
-        if self.page in self.selections:
-            for selection in self.selections[self.page]:
+        if str(self.page) in self.selections:
+            for selection in self.selections[str(self.page)]:
                 self.canvas.delete(selection['text'])
                 self.canvas.delete(selection['rect'])
 
@@ -22,29 +35,42 @@ class GUI:
         self.canvas.itemconfig(self.canvas_image, image=self.image)
         self.canvas.pack(side=BOTTOM)
 
-        if self.page in self.selections:
-            for selection in self.selections[self.page]:
-                new_rect, lbl_text = self.draw_rect(selection['bounds'], selection['cell'])
-
-                selection['rect'] = new_rect
-                selection['text'] = lbl_text
+        self.load_selections()
 
     def resize(self, event):
+        image = self.images[self.page - 1]
         self.size = (event.width, event.height)
-        scaled_image = self.images[self.page -
-                                   1].resize(self.size, Image.ANTIALIAS)
+        self.scale = (event.width / image.width, event.height / image.height)
+        scaled_image = image.resize(self.size, Image.ANTIALIAS)
         self.image = ImageTk.PhotoImage(scaled_image)
         self.canvas.itemconfig(self.canvas_image, image=self.image)
 
-    def on_right_click(self, event):
-        for selection in self.selections:
-            bounds = selection['bounds']
+        if str(self.page) in self.selections:
+            for selection in self.selections[str(self.page)]:
+                bounds = selection['bounds']
+                scaled_min = tuple(map(mul, bounds['min'], self.scale))
+                scaled_max = tuple(map(mul, bounds['max'], self.scale))
+                self.canvas.coords(selection['rect'], *scaled_max, *scaled_min)
 
-            if (event.x >= bounds['min'][0] and event.y >= bounds['min'][1] and event.x <= bounds['max'][0] and event.y <= bounds['max'][1]):
-                self.selections.remove(selection)
-                self.canvas.delete(selection['text'])
-                self.canvas.delete(selection['rect'])
-                break
+                x = scaled_min[0] + \
+                    (scaled_max[0] - scaled_min[0]) / 2
+                y = scaled_min[1] + \
+                    (scaled_max[1] - scaled_min[1]) / 2
+
+                self.canvas.coords(selection['text'], x, y)
+
+    def on_right_click(self, event):
+        if str(self.page) in self.selections:
+            for selection in self.selections[str(self.page)]:
+                bounds = selection['bounds']
+                scaled_min = tuple(map(mul, bounds['min'], self.scale))
+                scaled_max = tuple(map(mul, bounds['max'], self.scale))
+
+                if (event.x >= scaled_min[0] and event.y >= scaled_min[1] and event.x <= scaled_max[0] and event.y <= scaled_max[1]):
+                    self.selections[str(self.page)].remove(selection)
+                    self.canvas.delete(selection['text'])
+                    self.canvas.delete(selection['rect'])
+                    break
 
     def on_click(self, event):
         self.selection = {'min': (event.x, event.y)}
@@ -73,20 +99,28 @@ class GUI:
         return new_rect, lbl_text
 
     def add_rect(self):
-        if (not self.page in self.selections):
-            self.selections[self.page] = []
+        if (not str(self.page) in self.selections):
+            self.selections[str(self.page)] = []
         cell = self.cell.get("1.0", END+"-1c")
+        sheet = self.sheet.get("1.0", END+"-1c")
 
         new_rect, lbl_text = self.draw_rect(self.selection, cell)
+        scaled_min = tuple(map(div, self.selection['min'], self.scale))
+        scaled_max = tuple(map(div, self.selection['max'], self.scale))
+        bounds = {'min': (min(scaled_min[0], scaled_max[0]),
+                          min(scaled_min[1], scaled_max[1])),
+                  'max': (max(scaled_min[0], scaled_max[0]),
+                          max(scaled_min[1], scaled_max[1]))}
 
-        self.selections[self.page].append(
-            {'rect': new_rect, 'bounds': self.selection, 'cell': cell, 'text': lbl_text})
+        self.selections[str(self.page)].append(
+            {'rect': new_rect, 'bounds': bounds, 'sheet': sheet, 'cell': cell, 'text': lbl_text})
 
         self.canvas.delete(self.selection_rect)
         self.selection_rect = None
         self.selection = None
 
-    def __init__(self, root, images):
+    def __init__(self, root, images, config, save):
+        self.selections = config
         self.max_page = len(images)
         root.geometry('%dx%d' % (700, 900))
 
@@ -124,5 +158,13 @@ class GUI:
         self.lbl_cell.pack(in_=self.bottom, side=LEFT)
         self.cell = Text(self.bottom, height=1, width=5)
         self.cell.pack(in_=self.bottom, side=LEFT)
+        self.lbl_sheet = Label(root, text="Sheet:")
+        self.lbl_sheet.pack(in_=self.bottom, side=LEFT)
+        self.sheet = Text(self.bottom, height=1, width=15)
+        self.sheet.pack(in_=self.bottom, side=LEFT)
         self.btn_add = Button(root, text='Add', command=self.add_rect)
         self.btn_add.pack(in_=self.bottom, side=LEFT)
+        self.btn_save = Button(
+            root, text='Save', command=lambda: save(self.selections))
+        self.btn_save.pack(in_=self.bottom, side=LEFT)
+        self.load_selections()
--- src/ocrextract.py
+++ src/ocrextract.py
@@ -1,12 +1,18 @@
 import pytesseract
-import xlrd
 import fitz
 import io
+import sys
+import getopt
+import json
+import xlwings as xw
+import pandas as pd
 from tkinter import Tk
-from gui import GUI
 from PIL import Image
+from gui import GUI
 
 scale = 1
+file_name = 'data.json'
+
 
 def get_pdf_images(loc):
     pdf = fitz.open(loc)
@@ -26,15 +32,74 @@ def get_pdf_images(loc):
     return images
 
 
+def load():
+    try:
+        with open(file_name, 'r') as file:
+            return json.load(file)
+    except:
+        return {}
+
+
+def save(config):
+    with open(file_name, 'w') as file:
+        json.dump(config, file, indent=4)
+
+
+def usage():
+    print()
+
+
 def init():
-    images = get_pdf_images('input.pdf')
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'p:e:h:s', [
+            'pdf=', 'excel=', 'help', 'setup'])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-    root = Tk()
-    app = GUI(root, images)
-    root.mainloop()
+    pdf = 'input.pdf'
+    excel = 'output.xlsx'
+    setup = False
 
-    pass
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(2)
+        elif opt in ('-s', '--setup'):
+            setup = True
+        elif opt in ('-p', '--pdf'):
+            pdf = arg
+        elif opt in ('-e', '--excel'):
+            excel = arg
+        else:
+            usage()
+            sys.exit(2)
 
+    images = get_pdf_images(pdf)
+    config = load()
+
+    if setup:
+        root = Tk()
+        GUI(root, images, config, save)
+        root.mainloop()
+    else:
+        wb = xw.Book(excel)
+        last_rows = {}
+
+        for n, page in enumerate(images, start=1):
+            if str(n) in config:
+                selections = config[str(n)]
+
+                for selection in selections:
+                  sheet = wb.sheets[selection['sheet'] or 0]
+
+                  if not selection['sheet'] in last_rows:
+                    last_rows[selection['sheet']] = sheet.range(selection['cell'] + str(sheet.cells.last_cell.row)).end('up').row + 1
+                  last_row = last_rows[selection['sheet']]
+
+                  bounds = selection['bounds']
+                  crop = page.crop((*bounds['min'], *bounds['max']))
+                  sheet.range(selection['cell'] + str(last_row)).value = pytesseract.image_to_string(crop).strip()
 
 if __name__ == "__main__":
     init()
